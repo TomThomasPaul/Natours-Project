d81fd10cbe95953bfc4add9474b2df05c5b94896
TomThomasPaul <70032157+TomThomasPaul@users.noreply.github.com>
2021-05-12 19:44:17 -0400

    implemented cors in the app

--- app.js
+++ app.js
@@ -25,10 +25,12 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public'))); //this is to serve static files which cant be served through route
 
-app.use((req, res, next) => { //allow  react app in react course to get data from this node js server
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+// app.use((req, res, next) => { //allow  react app in react course to get data from this node js server
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
+app.use(cors()); //same as above code
+app.options('*', cors()); //allow patch ,delete requests for cross origin requests..patch,delete aare not simple requests so add this line
 app.use(express.json({limit : '10kb'})); //middleware needed when using POST..body parser..to facilitate req.body......
 app.use(cookieParser()); //enable cookies from browser to the app
 app.use(express.urlencoded({extended:true, limit:'10kb'})); //allow urlencoded data when submitting forms
